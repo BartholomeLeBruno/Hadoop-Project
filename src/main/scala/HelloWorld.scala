fffb333ab3c82066a09df7970f99c26a8dc4547b
Tilkeii <florian@gouarin.info>
2020-03-13 18:24:42 +0100

    theo corona

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -3,42 +3,54 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
+import java.time.Instant
 
 // define main method (Spark entry point)
 object HelloWorld {
   def main(args: Array[String]) {
 
-    // initialise spark context
-    val df = SparkSession.builder.getOrCreate.read.json("/Users/bartholome/Documents/tweets.json")
-    //df.show(3)
-    val text = df.select(df.col("text"))
-    val hashtags = df.select(df.col("entities.hashtags.text"))
-    val coordinate = df.select(df.col("coordinate"))
-    hashtags.show()
-    //df.printSchema()
-    //text.show(3)
-    //text.foreach(println(_))
-    //hashtags.show()
-
-    case class Tweets(corps: String, coordinate: String, hashtags: List[String])
-
-    
-
-    println("************")
     val conf = new SparkConf().setAppName(HelloWorld.getClass.getName)
     val spark: SparkSession = SparkSession.builder.config(conf).getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
 
-    // do stuff
-    println("************")
-    println("************")
-    println("Hello, world!")
-    val rdd = spark.sparkContext.parallelize(Array(1 to 10))
-    rdd.count()
-    println("************")
-    println("************")
+    /**
+      * Premiere partie pour tests en local sur l'edge
+      */
+    /*val df = spark.read.json("hdfs://master.sagean.fr:8020/user/hapoop/tweets")
+
+    df.show()
+
+    val text = df.select(col("text"))
+    val hashtags = df.select(explode(col("entities.hashtags.text")).alias("hashtag"))
+
+    hashtags.show()
+
+    hashtags
+      .groupBy("hashtag")
+      .count()
+      .orderBy(desc("count"))
+      .limit(10)
+      .show()*/
+
+    /**
+      * Deuxieme partie fonctionnelle sur l'hdfs
+      */
+    val tweetsDirDf = spark.read.json("hdfs://master.sagean.fr:8020/user/hapoop/tweets_dir")
+    var tweetsLastHour = tweetsDirDf.filter("time > " + String.valueOf(Instant.now().getEpochSecond() - 3600))
+
+    val hashtags = tweetsLastHour.select(explode(col("entities.hashtags.text")).alias("hashtag"))
+  
+    val topTenHashtags = hashtags
+      .groupBy("hashtag")
+      .count()
+      .orderBy(desc("count"))
+      .limit(10)
+
+    topTenHashtags.write.json(
+      "hdfs://master.sagean.fr:8020/user/hapoop/toptweets/time=" + String.valueOf(Instant.now().getEpochSecond())
+    )
 
     // terminate spark context
     spark.stop()
-
   }
 }
